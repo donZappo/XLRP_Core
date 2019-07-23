f9d31dac8987540371df002e3c2f194adfcf7db3
Haree78 <51842596+Haree78@users.noreply.github.com>
2019-07-23 12:37:56 +0100

    UI Change for Ace Pilot change
    When moving after shooting the floatie shall read "MASTER TACTICIAN", not "ACE PILOT".

---
+++ Extended_CE/SkillChanges.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+using Localize;
+
+namespace Extended_CE.SkillChanges
+{
+    // If we are adding a floatie for Ace Pilot because we moved after shooting, change it to Master Tactician because of our skill changes
+    [HarmonyPatch(typeof(MessageCenter), "PublishMessage")]
+    public static class MessageCenter_PublishMessage
+    {
+        public static void Prefex(ref MessageCenterMessage message)
+        {
+            if(message is FloatieMessage floatie)
+            {
+                if(floatie.text.ToString() == "ACE PILOT")
+                {
+                    floatie.SetText(new Text("MASTER TACTICIAN", new object[0]));
+                }
+            }
+        }
+    }
+}
